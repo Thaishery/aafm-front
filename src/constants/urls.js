5d199eacc1bf38fc079a07f835e0e6e92cb91d71
Thaishery <gdeb@gdeb.fr>
2024-03-22 14:57:50 +0100

    fix url

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,11 +1,14 @@
-const port = "8000";
-// const port = "8081";
+// const port = "8000";
+const port = "8081";
 const url = {
   /* Localhost */
-  apiUrl: `http://localhost:${port}`,
+  // apiUrl: `http://localhost:${port}`,
 
   // pprod
   // apiUrl: `https://pprod.aafm.gdeb.fr:${port}`
+  
+  // prod
+  // apiUrl: `https://aafm-grigny.fr:${port}`
   
 };
 
